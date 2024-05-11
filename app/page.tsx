3650a809a8f0c99ef7d2e0781321a4745739c506
JessieJessJe <jiahuihan@yahoo.com>
2024-05-11 15:29:58 -0400

    tweak

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="mt-20 max-md:mt-10 max-md:max-w-full">
+      <div className="mt-20 max-md:mt-0 max-md:max-w-full">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
           <div className="flex flex-col w-[45%]  max-md:ml-0 max-md:w-full">
             <img
