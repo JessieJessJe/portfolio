d09dbeb16a3673fd9e1aa4a7ad29c0094ab97bb4
JessieJessJe <jiahuihan@yahoo.com>
2024-05-11 15:19:55 -0400

    tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -31,23 +31,18 @@ const RainbowText = ({ text }) => {
 export default function Home() {
   return (
     <div className="font-header flex flex-col p-6 bg-black max-md:px-5">
-      <div className="flex gap-2.5 justify-between w-full text-2xl text-slate-100 font-[274] max-md:flex-wrap max-md:max-w-full">
+      <div className="flex gap-2.5 justify-between w-full text-2xl text-slate-100 font-[274] max-md:flex-wrap w-full lg:w-400">
         <div>Jessie Han</div>
         <div className="flex gap-5 whitespace-nowrap">
           <a className="hover:underline" href="https://read.cv/jessiehanvana" target="_blank" rel="noopener noreferrer">Resume</a>
           <a className="hover:underline" href="https://linkedin.com/in/jiahuihan" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
       </div>
-      <div className="font-main mt-40 text-5xl leading-tight text-slate-100 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-        I am a <RainbowText text="product designer"/> that translate data and AI insights<br />
-        into impactful user experiences.
+      <div className="font-main w-full lg:w-[72%] mt-40 text-4xl lg:text-5xl leading-tight text-slate-100 max-md:mt-10">
+        I am a <RainbowText text="product designer"/> that translate data and AI insights into impactful user experiences.
       </div>
-      <div className="font-main mt-16 text-5xl leading-tight text-slate-100 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-        I thrive on solving <RainbowText text="complex problems"/>, whether it means
-        <br />
-        extensive domain research, iterative design blending diverse
-        <br />
-        inspirations or tweaking every detail on the prototype.
+      <div className="font-main w-full lg:w-[72%] mt-16 text-4xl lg:text-5xl leading-tight text-slate-100 max-md:mt-10">
+        I thrive on solving <RainbowText text="complex problems"/>, whether it means extensive domain research, iterative design blending diverse inspirations or tweaking every detail on the prototype.
       </div>
       <div className="mt-48 border border-gray-500 border-solid max-md:mt-10 max-md:max-w-full overflow-hidden">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
@@ -69,16 +64,12 @@ export default function Home() {
                   Case Study on Notion &#8599; 
                 </a>
               </div>
-            
-            
-            
-            
             </div>
           </div>
         </div>
       </div>
       <div className="mt-44 border border-gray-500 border-solid max-md:mt-10 max-md:max-w-full overflow-hidden">
-        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+        <div className="flex gap-5 max-md:flex-col-reverse max-md:gap-0">
           <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
             <div className="flex flex-col justify-center px-6 py-16 text-slate-100 max-md:px-5 max-md:max-w-full">
               <div className="font-title leading-tight text-3xl md:text-3xl lg:text-4xl max-md:max-w-full">
@@ -115,13 +106,13 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="font-title self-start mt-44 text-3xl md:text-3xl lg:text-4xl text-slate-100 max-md:mt-10 max-md:max-w-full">
+      <div className="font-title self-start mt-44 text-3xl md:text-3xl lg:text-4xl text-slate-100 max-md:mt-24 max-md:max-w-full">
         Other Works / Selected Projects
       </div>
       <div className="font-body self-start mt-2.5 text-xl text-slate-100 max-md:max-w-full">
         Data Visualization / Prototyping / Graphic Design
       </div>
-      <div className="mt-20 max-md:pr-5 max-md:mt-10 max-md:max-w-full">
+      <div className="mt-20 max-md:mt-10 max-md:max-w-full">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
           <div className="flex flex-col w-[58%] max-md:ml-0 max-md:w-full">
             <img
@@ -155,6 +146,11 @@ export default function Home() {
               className="object-cover w-full max-md:mt-6 max-md:max-w-full"
             />
           </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-end font-header text-slate-100 font-[274] my-10">
+        <div> 
+          © 2024 Jessie Han | Till next time :)
         </div>
       </div>
     </div>
